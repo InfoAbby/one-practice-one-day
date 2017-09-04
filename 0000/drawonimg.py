7756b74b53a484a1b29d8311dffb75034341b5ee
Abby <infoabby@foxmail.com>
2017-09-04 11:27:53 +0800

    perfect 0000

--- 0000/drawonimg.py
+++ 0000/drawonimg.py
@@ -1,23 +1,19 @@
 from PIL import Image,ImageDraw,ImageFont
-
 import sys
-
 import random
 
+def add_num(img):
+    draw = ImageDraw.Draw(img)
+
+    ft = ImageFont.truetype("C:\\Windows\Fonts\Arial.ttf",size=150)
+    width,height=img.size
+    draw.text((width-170,0),'99',font=ft,fill='red')
+
+    img.show()
 
 
-txt = '5'
+if __name__ == '__main__':
+    img = Image.open('image.jpg')
+    add_num(img)
+    print('0')
 
-im = Image.open('image.jpg')
-
-ft = ImageFont.truetype("C:\\Windows\Fonts\STZHONGS.TTF",150)
-
-draw = ImageDraw.Draw(im)
-
-
-
-draw.text((480,0),txt,font=ft,fill='red')
-
-
-
-im.show()
