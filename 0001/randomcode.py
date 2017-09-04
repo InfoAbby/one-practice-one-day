707a89b18f577e7145156e52e776404e1a97beb0
Abby <infoabby@foxmail.com>
2017-09-04 12:12:25 +0800

    first 0002 laji

--- 0001/randomcode.py
+++ 0001/randomcode.py
@@ -1,7 +1,17 @@
 import random
 import string
-
+from sqlalchemy import Column,String,create_engine
+import pymysql.cursors
+from config import config
 #激活码中的字符和数字
+
+db = pymysql.connect("localhost","root","748134","practice")
+cursor = db.cursor()
+
+
+
+
+
 
 field = string.ascii_letters+string.digits
 
@@ -16,3 +26,12 @@ def generate(n):
 
 if __name__ == '__main__':
     print(generate(10))
+    for g in generate(10):
+        sql = "INSERT INTO p0001(code) VALUES ('%s')" % g
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        db.rollback()
+    db.close()
+
